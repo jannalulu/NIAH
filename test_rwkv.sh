a36035bf788fcde3477f182465bef998da8ed0b9
Jellyfish042 <1604466216@qq.com>
2024-12-16 20:17:02 +0800

    eval rwkv

---
+++ test_rwkv.sh
@@ -0,0 +1,13 @@
+python eval_rwkv.py \
+    --tokenized PY007/tokenized_proof_pile_test_neox \
+    --dataset-min-tokens 32768 \
+    --samples 20 \
+    --output-file data/rwkv_7.csv \
+    --min-tokens 2048 \
+    --max-tokens 12288 \
+    --tokens-step 2048 \
+    --save-tokenized eval_dataset \
+    --truncate \
+    -m RWKV-x070-Pile-168M-20241120-ctx4096 \
+    -m RWKV-x070-Pile-421M-20241127-ctx4096 \
+    -m RWKV-x070-Pile-1.47B-20241210-ctx4096
