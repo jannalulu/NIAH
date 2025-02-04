c0d9ba3de1a7a9ea6b5952a1b6272b3387fde9d4
jannalulu <jannaluzj01@gmail.com>
2025-02-04 19:58:36 +0000

    path to models.pth

--- pass_key.py
+++ pass_key.py
@@ -16,7 +16,8 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
 import seaborn as sns
 
-model_path = "../.cache/huggingface/hub/rwkv7-1.5B-world-smerky-ft/snapshots/848422f82e020c2b6c4deb43029afd62dc102e23"
+model_path_0 = "/workspace/RWKV-block/test/v7_goose/.hf_build/rwkv-0/"
+model_path_1 = "/workspace/RWKV-block/test/v7_goose/.hf_build/rwkv-1/"
 
 def get_gpu_memory():
     """Returns the current GPU memory usage in MB."""
@@ -29,7 +30,7 @@ def parse_config():
     parser.add_argument('--cache_dir', type=str, default="./cache")
 
     parser.add_argument('--min_tokens', type=int, default=16384, help='minimum token length to start evaluation')
-    parser.add_argument('--max_tokens', type=int, default=65536, help='maximum token length for evaluation')
+    parser.add_argument('--max_tokens', type=int, default=32768, help='maximum token length for evaluation')
     parser.add_argument('--interval', type=int, default=2048, help='interval for evaluation')
     parser.add_argument('--num_tests', type=int, default=3, help='number of repeat testing for each length')
     parser.add_argument('--min_depth', type=float, default=0.3, help='minimum depth ratio to start testing')
@@ -125,7 +126,7 @@ def main(args):
     print("base model", args.base_model)
 
     # Load model and tokenizer
-    model = AutoModelForCausalLM.from_pretrained('m8than/rwkv7-1b5-64k', trust_remote_code=True)
+    model = AutoModelForCausalLM.from_pretrained(model_path_0, trust_remote_code=True, tmix_backend="cuda")
     model = model.to('cuda')
     tokenizer = AutoTokenizer.from_pretrained('fla-hub/rwkv7-1.5B-world', trust_remote_code=True)
 
